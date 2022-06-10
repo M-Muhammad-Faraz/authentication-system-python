02d49f5bf5e7d8b6fbfc8ba623beed8b3cbc708b
Black Clover <mianmuhammadfaraz2@gmail.com>
2022-06-10 22:35:04 +0500

    1.1.1

--- main.py
+++ main.py
@@ -3,38 +3,31 @@ from login import login
 from signup import signup
 
 
-
+PROGRAM_END = False
 option = {
-    "login":1,
-    "signup":2,
-    "exit":3
+    "login":"1",
+    "signup":"2",
+    "exit":"3"
 }
 def home_page():
     system("cls")
-    choice=int(input("Authorization\nPress 1: Login\nPress 2: Signup\n"))
+    choice=input("Authorization\nPress 1: Login\nPress 2: Signup\nPress 3: Exit\n")
     if(choice == option["login"]):
-        
         success=login()
         if success == -1:
             print("Login Failed. Wrong Username/Password. Try Again")
-        
         else:
             success.main_menu()
-
     elif choice == option["signup"]:
         signup()
     elif choice == option["exit"]:
-        exit;
+        global PROGRAM_END
+        PROGRAM_END=True
     else:
-        input("Wrong Option Selected...\nPress Any Key To Continue...\n")
+        input("Wrong Option Selected...\nPress Any Key To Retry..\n")
         system("cls")    
 
-
-
-
-
-
-while True:
+while not PROGRAM_END:
     home_page()
     
   
--- signup.py
+++ signup.py
@@ -1,14 +1,22 @@
 from os import system
+import re
 from user import user
 from database import database;
 
+
+
+def verifyInput(inp):
+  if inp == 'p'or inp == 'P'or inp == 'n'or inp == 'N':
+    return True
+  else:
+    return False
 
 def signup():
     system("cls")
     print("SIGNUP PAGE\n")
     username=input("Enter Username: ")
     passwordNowVerified = False 
-
+    input_now_verified = False
     while not passwordNowVerified:
         password=input("Enter Password: ")
         confirmPassword=input("Enter Password again: ")
@@ -17,23 +25,62 @@ def signup():
         else:
             print("Password Dont Match Try Again\n")
         
+    input_now_verified = False
+    while not input_now_verified:
+      answer = input("Heart Attack... Press 'P' for positive. Press 'N' for Negitive : ")
+      if verifyInput(answer):
+        input_now_verified=True
+        heart_attack = True if (answer=='p' or answer=='P') else False
+      else:
+        print("Wrong Input Try Again\n")
+    
+    input_now_verified = False
+    while not input_now_verified:
+      answer = input("Tumor... Press 'P' for positive. Press 'N' for Negitive : ")
+      if verifyInput(answer):
+        input_now_verified=True
+        tumor = True if (answer=='p' or answer=='P') else False
+      else:
+        print("Wrong Input Try Again\n")
 
-    answer = input("Heart Attack... Press 'P' for positive. Press 'N' for Negitive : ")
-    heart_attack = True if (answer=='p' or answer=='P') else False
+    input_now_verified = False
+    while not input_now_verified:
+      answer = input("Cancer... Press 'P' for positive. Press 'N' for Negitive : ")
+      if verifyInput(answer):
+        input_now_verified=True
+        cancer = True if (answer=='p' or answer=='P') else False
+      else:
+        print("Wrong Input Try Again\n")
+
+    input_now_verified = False
+    while not input_now_verified:
+      answer = input("COVID... Press 'P' for positive. Press 'N' for Negitive : ")
+      if verifyInput(answer):
+        input_now_verified=True
+        covid = True if (answer=='p' or answer=='P') else False
+      else:
+        print("Wrong Input Try Again\n")
+
+
+    input_now_verified = False
+    while not input_now_verified:
+      answer = input("TB... Press 'P' for positive. Press 'N' for Negitive : ")
+      if verifyInput(answer):
+        input_now_verified=True
+        tb = True if (answer=='p' or answer=='P') else False
+      else:
+        print("Wrong Input Try Again\n")
     
-    answer = input("Tumor... Press 'P' for positive. Press 'N' for Negitive : ")
-    tumor = True if (answer=='p' or answer=='P') else False
-    
-    answer = input("Cancer... Press 'P' for positive. Press 'N' for Negitive : ")
-    cancer = True if (answer=='p' or answer=='P') else False
-    
-    answer = input("COVID... Press 'P' for positive. Press 'N' for Negitive : ")
-    covid = True if (answer=='p' or answer=='P') else False
-    
-    answer = input("TB... Press 'P' for positive. Press 'N' for Negitive : ")
-    tb = True if (answer=='p' or answer=='P') else False
-    
-    bloodgroup = input("Enter You bloodgroup : ")
+
+    input_now_verified = False
+    while not input_now_verified:
+
+      bloodgroup = input("Enter You bloodgroup : ")
+
+      if re.match("(A|B|AB|O)[+-]",bloodgroup):
+        input_now_verified=True
+      else:
+        input("Incorrect Bloodgroup, Please try again...\n")
 
     newUser= user(username,password,{"heartAttack":heart_attack,"tumor":tumor,"cancer":cancer,"covid":covid,"tb":tb,"bloodGroup":bloodgroup})
     database.addANewUser(newUser)
